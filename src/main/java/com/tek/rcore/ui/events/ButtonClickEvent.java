487f96d8f7d80cf6bf5a827b26072049eef078d3
RedstoneTek <xxredstonetekxx@gmail.com>
2019-07-27 17:03:55 -0400

    Commented/Documented both events

--- src/main/java/com/tek/rcore/ui/events/ButtonClickEvent.java
+++ src/main/java/com/tek/rcore/ui/events/ButtonClickEvent.java
@@ -6,14 +6,35 @@ import org.bukkit.inventory.ItemStack;
 
 import com.tek.rcore.ui.InterfaceState;
 
+/**
+ * A class which represents a
+ * click on a {@link ButtonComponent}.
+ * 
+ * @author RedstoneTek
+ */
 public class ButtonClickEvent {
 	
+	//The ButtonClickEvent values
 	private Player player;
 	private InterfaceState interfaceState;
 	private ClickType clickType;
 	private ItemStack item;
 	private int x, y;
 	
+	/**
+	 * Creates a ButtonClickEvent with
+	 * the specified values.
+	 * <p>NOTE: The X and Y coordinates are defined relative to the component's X and Y position.
+	 * For instance, if the X position of the component is X5 and the user clicks at X6, the X coordinate
+	 * passed would be X1 (X6-X5=X1)</p>
+	 * 
+	 * @param player The player who clicked
+	 * @param interfaceState The interface state clicked
+	 * @param clickType The click type
+	 * @param item The cursor item
+	 * @param x The relative X position from the left (Starts at 0, see above)
+	 * @param y The relative Y position from the top (Starts at 0, see above)
+	 */
 	public ButtonClickEvent(Player player, InterfaceState interfaceState, ClickType clickType, ItemStack item, int x, int y) {
 		this.player = player;
 		this.interfaceState = interfaceState;
@@ -23,26 +44,56 @@ public class ButtonClickEvent {
 		this.y = y;
 	}
 
+	/**
+	 * Gets the player who clicked.
+	 * 
+	 * @return The player who clicked
+	 */
 	public Player getPlayer() {
 		return player;
 	}
 	
+	/**
+	 * Gets the interface state clicked.
+	 * 
+	 * @return The interface state
+	 */
 	public InterfaceState getInterfaceState() {
 		return interfaceState;
 	}
 	
+	/**
+	 * Gets the click type.
+	 * 
+	 * @return The click type
+	 */
 	public ClickType getClickType() {
 		return clickType;
 	}
 	
+	/**
+	 * Gets the item in the cursor.
+	 * 
+	 * @return The cursor item
+	 */
 	public ItemStack getItem() {
 		return item;
 	}
 	
+	/**
+	 * Gets the relative X position from the left.
+	 * 
+	 * @return The relative X position
+	 */
 	public int getX() {
 		return x;
 	}
 	
+	/**
+	 * Gets the relative Y position from the top.
+	 * 
+	 * @return The relative Y position
+	 */
 	public int getY() {
 		return y;
 	}
--- src/main/java/com/tek/rcore/ui/events/InterfaceCloseEvent.java
+++ src/main/java/com/tek/rcore/ui/events/InterfaceCloseEvent.java
@@ -4,34 +4,70 @@ import org.bukkit.entity.Player;
 
 import com.tek.rcore.ui.InterfaceState;
 
+/**
+ * A class which represents
+ * the closing of an interface.
+ * 
+ * @author RedstoneTek
+ */
 public class InterfaceCloseEvent {
 	
+	//The InterfaceCloseEvent values
 	private Player player;
 	private InterfaceCloseType closeType;
 	private InterfaceState interfaceState;
 	
+	/**
+	 * Creates an InterfaceCloseEvent with
+	 * the specified values.
+	 * 
+	 * @param player The player
+	 * @param closeType The close type
+	 * @param interfaceState The interface state closed
+	 */
 	public InterfaceCloseEvent(Player player, InterfaceCloseType closeType, InterfaceState interfaceState) {
 		this.player = player;
 		this.closeType = closeType;
 		this.interfaceState = interfaceState;
 	}
 
+	/**
+	 * Gets the player who closed the interface.
+	 * 
+	 * @return The player
+	 */
 	public Player getPlayer() {
 		return player;
 	}
 
+	/**
+	 * Gets the close type of the event.
+	 * 
+	 * @return The close type
+	 */
 	public InterfaceCloseType getCloseType() {
 		return closeType;
 	}
 	
+	/**
+	 * Gets the interface state closed.
+	 * 
+	 * @return The interface state
+	 */
 	public InterfaceState getInterfaceState() {
 		return interfaceState;
 	}
 	
+	/**
+	 * An enum representing the
+	 * different types of close types.
+	 * 
+	 * @author RedstoneTek
+	 */
 	public static enum InterfaceCloseType {
-		PLAYER,
-		PROGRAMMATICAL,
-		IGNORED;
+		PLAYER, //The interface was closed by the player
+		PROGRAMMATICAL, //The interface was closed programmatically via code
+		IGNORED; //The interface was closed because of some weird minecraft behaviour, ignore it
 	}
 	
 }
